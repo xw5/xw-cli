d8b488e4b4103804f374b1c946f2c1b1ea755e27
xiewu <xiewu@kingsoft.com>
2018-09-12 14:18:53 +0800

    增加配置修改与优化介绍相关地址

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const download = require('download-git-repo');//下载库
 //const downloadSuper = require('github-download-parts');//下载库，支持下载项目下的文件夹
 const figlet = require('figlet');//输出特殊字符
 
-let isReplace = false;//是否是替换文件状态
+let isReplace = false;
 let existName = '';//重复的工程名
 let backUpName = '';//备份的工程名
 
 //设置版本号
-commander.version('v1.0.0','-v,--version');
+commander.version('v1.0.3','-v,--version');
 
 //新增option --name后面的[val]]当前这个选项的参数值 []可选<>必填
 //如果第三个参数为一个函数，会接受来处用户的值并返回一个值做为实际的值
-commander.option('-n,--name [name]','设置名称',val => val.toLowerCase(),'zmouse');
+//commander.option('-n,--name [name]','设置名称',val => val.toLowerCase(),'zmouse');
 
 //添加子命令
 let createCommnader = commander.command('create <project-name>');
@@ -30,45 +30,60 @@ createCommnader.action((projectName)=>{
 //解析来自process.argv上的数据
 commander.parse(process.argv);
 
-
 //如果有name值就输出
-if(commander.name){
-    console.log(commander.name);
-    shelljs.sed('-i','sass',commander.name,'test.js');
-    shelljs.mv('test0.js','test.js');
-}
-//{ jsmodule: 'requirejs', cssmodule: 'less', version: 'vmd5' }
-//下载模板逻辑实现
+// if(commander.name){
+//     console.log(commander.name);
+//     shelljs.sed('-i','sass',commander.name,'test.js');
+//     shelljs.mv('test0.js','test.js');
+// }
+//{ jsmodule: 'requirejs', cssmodule: 'less', version: 'vmd5' }//下载模板逻辑实现
 function downloadTemplate(dir,answers){
     const loadingCli = ora(chalk.gray(`Loading ${dir},请稍等...\r\n`)).start();
-    download('github:xw5/devEnv_multipage',dir+'/',(err)=>{
+    download('github:xw5/wps-cli-dir',dir+'/',(err)=>{
         //删除git配置
         shelljs.rm(dir+'/.gitignore');
-        //css整理
-        answers.cssmodule === 'less' ? shelljs.rm(dir+'/src/sass') : shelljs.rm(dir+'/src/less');
-        //js整理
+        //css解析方式处理
+        if(answers.cssmodule === 'less'){
+            shelljs.rm('-rf',dir+'/src/sass')
+        }else{
+            shelljs.rm('-rf',dir+'/src/less');
+            writeConfig('less','sass',dir);
+        }
+        //js模块化开发方式配置
         switch(answers.jsmodule){
             case 'requirejs':
                 shelljs.mv(dir+'/src/js-require',dir+'/src/js');
-                shelljs.rm(dir+'/src/js-es6');
-                shelljs.rm(dir+'/src/js-webpack');
+                shelljs.rm('-rf',dir+'/src/js-es6');
+                shelljs.rm('-rf',dir+'/src/js-webpack');
+                shelljs.rm(dir+'/package.es6.json');
+                shelljs.rm(dir+'/webpack.config.es6.js');
                 shelljs.rm(dir+'/src/index.es6.webpack.html');
                 shelljs.mv(dir+'/src/index.require.html',dir+'/src/index.html');
                 break;
             case 'webpack':
                 shelljs.mv(dir+'/src/js-webpack',dir+'/src/js');
-                shelljs.rm(dir+'/src/js-require');
-                shelljs.rm(dir+'/src/js-es6');
+                shelljs.rm('-rf',dir+'/src/js-require');
+                shelljs.rm('-rf',dir+'/src/js-es6');
+                shelljs.rm(dir+'/package.es6.json');
+                shelljs.rm(dir+'/webpack.config.es6.js');
                 shelljs.rm(dir+'/src/index.require.html');
-                shelljs.mv(dir+'/src/index.es6.webpack.html',dir+'/src/index.html')
+                shelljs.mv(dir+'/src/index.es6.webpack.html',dir+'/src/index.html');
+                writeConfig('requirejs','webpack',dir);
                 break;
             case 'es6':
                 shelljs.mv(dir+'/src/js-es6',dir+'/src/js');
-                shelljs.rm(dir+'/src/js-require');
-                shelljs.rm(dir+'/src/js-webpack');
+                shelljs.rm('-rf',dir+'/src/js-require');
+                shelljs.rm('-rf',dir+'/src/js-webpack');
+                shelljs.mv(dir+'/package.es6.json',dir+'/package.json');
+                shelljs.mv(dir+'/webpack.config.es6.js',dir+'/webpack.config.js');
                 shelljs.rm(dir+'/src/index.require.html');
-                shelljs.mv(dir+'/src/index.es6.webpack.html',dir+'/src/index.html')
+                shelljs.mv(dir+'/src/index.es6.webpack.html',dir+'/src/index.html');
+                writeConfig('requirejs','webpack',dir);
                 break;
+        }
+        //根据选择不同的版本号切换配置文件
+        if(answers.version === 'rename'){
+            writeConfig('vmd5','rename',dir);
         }
 
         figlet('WPS-CLI', function(err,data){
@@ -76,8 +91,8 @@ function downloadTemplate(dir,answers){
                 console.log(chalk.green(data));
             }
             loadingCli.clear();
-            loadingCli.succeed(chalk.green(`${dir}项目模板已下载成功！`));
             console.log('\r\n');
+            loadingCli.succeed(chalk.green(`${dir}项目模板已下载成功！`));
             console.log(chalk.blue('------------------项目运行步骤说明start---------------------'));
             console.log('\r\n');
             console.log(chalk.blue(`    1.cd ${dir}`));
@@ -87,6 +102,10 @@ function downloadTemplate(dir,answers){
             console.log(chalk.blue('------------------项目运行步骤说明end-----------------------'));
         });
     });
+}
+//修改配置文件
+function writeConfig(oldStr,newStr,dir){
+    shelljs.sed('-i',oldStr,newStr,dir+'/config/config.js');
 }
 
 //判断文件夹是否存在处理逻辑
@@ -136,6 +155,7 @@ function slectActionBefore(name){
                     slectAction(existName);
                     break;
                 case 'replace'://直接替换原项目
+                    isReplace = true;
                     slectAction(existName);
                     break;
                 default:
@@ -187,6 +207,9 @@ function slectAction(name){
         }],
         default:0
     }]).then(answers => {
+        if(isReplace){
+            shelljs.rm('-rf',existName);
+        }
         downloadTemplate(name,answers);
     })
 }
