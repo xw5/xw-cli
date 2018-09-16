3f63c569e539e1a7349bee02cdc083aaf975e28a
xw5 <412832768@qq.com>
2018-09-16 23:47:23 +0800

    增加混合模板下载功能

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ let existName = '';//重复的工程名
 let backUpName = '';//备份的工程名
 
 //设置版本号
-commander.version('v1.0.9','-v,--version');
+commander.version('v1.1.0','-v,--version');
 
 //新增option --name后面的[val]]当前这个选项的参数值 []可选<>必填
 //如果第三个参数为一个函数，会接受来处用户的值并返回一个值做为实际的值
 //commander.option('-n,--name [name]','设置名称',val => val.toLowerCase(),'zmouse');
+commander.option('-s,--separate','使用前后端分离版本');
 
 //添加子命令
 let createCommnader = commander.command('create <project-name>');
@@ -36,74 +37,93 @@ commander.parse(process.argv);
 //     shelljs.sed('-i','sass',commander.name,'test.js');
 //     shelljs.mv('test0.js','test.js');
 // }
-//{ jsmodule: 'requirejs', cssmodule: 'less', version: 'vmd5' }//下载模板逻辑实现
-function downloadTemplate(dir,answers){
+//{ jsmodule: 'requirejs', cssmodule: 'less', version: 'vmd5' }
+
+//下载模板逻辑
+function downloadTemplate(dir,answers,gitName){
     const loadingCli = ora(chalk.gray(`Loading ${dir},请稍等...\r\n`)).start();
-    download('github:xw5/wps-cli-dir',dir+'/',(err)=>{
+    download('github:xw5/'+gitName,dir+'/',(err)=>{
+        if(err){
+            console.log('网络繁忙，请稍后再试！');
+            return;
+        }
         //删除git配置
         shelljs.rm(dir+'/.gitignore');
-        //css解析方式处理
-        if(answers.cssmodule === 'less'){
-            shelljs.rm('-rf',dir+'/src/sass')
+        if(commander.separate){
+            //css解析方式处理
+            if(answers.cssmodule === 'less'){
+                shelljs.rm('-rf',dir+'/src/sass');
+            }else{
+                shelljs.rm('-rf',dir+'/src/less');
+                writeConfig('less','sass',dir);
+            }
+            //js模块化开发方式配置
+            switch(answers.jsmodule){
+                case 'requirejs':
+                    shelljs.mv(dir+'/src/js-require',dir+'/src/js');
+                    shelljs.rm('-rf',dir+'/src/js-es6');
+                    shelljs.rm('-rf',dir+'/src/js-webpack');
+                    shelljs.rm(dir+'/package.es6.json');
+                    shelljs.rm(dir+'/webpack.config.es6.js');
+                    shelljs.rm(dir+'/src/index.es6.webpack.html');
+                    shelljs.mv(dir+'/src/index.require.html',dir+'/src/index.html');
+                    break;
+                case 'webpack':
+                    shelljs.mv(dir+'/src/js-webpack',dir+'/src/js');
+                    shelljs.rm('-rf',dir+'/src/js-require');
+                    shelljs.rm('-rf',dir+'/src/js-es6');
+                    shelljs.rm(dir+'/package.es6.json');
+                    shelljs.rm(dir+'/webpack.config.es6.js');
+                    shelljs.rm(dir+'/src/index.require.html');
+                    shelljs.mv(dir+'/src/index.es6.webpack.html',dir+'/src/index.html');
+                    writeConfig('requirejs','webpack',dir);
+                    break;
+                case 'es6':
+                    shelljs.mv(dir+'/src/js-es6',dir+'/src/js');
+                    shelljs.rm('-rf',dir+'/src/js-require');
+                    shelljs.rm('-rf',dir+'/src/js-webpack');
+                    shelljs.mv(dir+'/package.es6.json',dir+'/package.json');
+                    shelljs.mv(dir+'/webpack.config.es6.js',dir+'/webpack.config.js');
+                    shelljs.rm(dir+'/src/index.require.html');
+                    shelljs.mv(dir+'/src/index.es6.webpack.html',dir+'/src/index.html');
+                    writeConfig('requirejs','webpack',dir);
+                    break;
+            }
+            //根据选择不同的版本号切换配置文件
+            if(answers.version === 'rename'){
+                writeConfig('vmd5','rename',dir);
+            }
         }else{
-            shelljs.rm('-rf',dir+'/src/less');
-            writeConfig('less','sass',dir);
+            if(!answers.template){//如果不需要HTML模块化，则去除不需要相关文件
+                shelljs.rm('-rf',dir+'/src/template');
+                shelljs.rm('-rf',dir+'/src/index.template.html');
+            }else{
+                shelljs.mv(dir+'/src/index.template.html',dir+'/src/index.html');
+                writeConfig('template:false','template:true',dir);
+            }
         }
-        //js模块化开发方式配置
-        switch(answers.jsmodule){
-            case 'requirejs':
-                shelljs.mv(dir+'/src/js-require',dir+'/src/js');
-                shelljs.rm('-rf',dir+'/src/js-es6');
-                shelljs.rm('-rf',dir+'/src/js-webpack');
-                shelljs.rm(dir+'/package.es6.json');
-                shelljs.rm(dir+'/webpack.config.es6.js');
-                shelljs.rm(dir+'/src/index.es6.webpack.html');
-                shelljs.mv(dir+'/src/index.require.html',dir+'/src/index.html');
-                break;
-            case 'webpack':
-                shelljs.mv(dir+'/src/js-webpack',dir+'/src/js');
-                shelljs.rm('-rf',dir+'/src/js-require');
-                shelljs.rm('-rf',dir+'/src/js-es6');
-                shelljs.rm(dir+'/package.es6.json');
-                shelljs.rm(dir+'/webpack.config.es6.js');
-                shelljs.rm(dir+'/src/index.require.html');
-                shelljs.mv(dir+'/src/index.es6.webpack.html',dir+'/src/index.html');
-                writeConfig('requirejs','webpack',dir);
-                break;
-            case 'es6':
-                shelljs.mv(dir+'/src/js-es6',dir+'/src/js');
-                shelljs.rm('-rf',dir+'/src/js-require');
-                shelljs.rm('-rf',dir+'/src/js-webpack');
-                shelljs.mv(dir+'/package.es6.json',dir+'/package.json');
-                shelljs.mv(dir+'/webpack.config.es6.js',dir+'/webpack.config.js');
-                shelljs.rm(dir+'/src/index.require.html');
-                shelljs.mv(dir+'/src/index.es6.webpack.html',dir+'/src/index.html');
-                writeConfig('requirejs','webpack',dir);
-                break;
-        }
-        //根据选择不同的版本号切换配置文件
-        if(answers.version === 'rename'){
-            writeConfig('vmd5','rename',dir);
-        }
-
+        //项目提示
         figlet('WPS-CLI', function(err,data){
             if(!err){
                 console.log(chalk.green(data));
-            }
+            };
             loadingCli.clear();
             console.log('\r\n');
             loadingCli.succeed(chalk.green(`${dir}项目模板已下载成功！`));
             console.log(chalk.blue('------------------项目运行步骤说明start---------------------'));
             console.log('\r\n');
-            console.log(chalk.blue(`    1.cd ${dir}`));
-            console.log(chalk.blue(`    2.npm install`));
-            console.log(chalk.blue(`    3.npm run dev`));
+            console.log(chalk.blue(`    cd ${dir}`));
+            console.log(chalk.blue(`    npm install //安装项目模块`));
+            !commander.separate && console.log(chalk.blue(`    npm run static //走静态开发模式`));
+            console.log(chalk.blue(`    npm run dev //走${!commander.separate ? '混合' : ''}开发流程`));
+            console.log(chalk.blue(`    npm run build //走构建流程`));
             console.log('\r\n');
             console.log(chalk.blue('------------------项目运行步骤说明end-----------------------'));
         });
     });
 }
-//修改配置文件
+
+//修改配置文件相关配置参数
 function writeConfig(oldStr,newStr,dir){
     shelljs.sed('-i',oldStr,newStr,dir+'/config/config.js');
 }
@@ -165,51 +185,74 @@ function slectActionBefore(name){
     }
 }
 
+//下载封装
+function downloadPro(name,answers,proName){
+    if(isReplace){
+        shelljs.rm('-rf',existName);
+    }
+    downloadTemplate(name,answers,proName);
+}
+
 //用户选择模板选项逻辑
 function slectAction(name){
-    inquirer.prompt([{
-        type:'list',
-        name:'jsmodule',
-        message:chalk.red.bgYellow('你使用的模块化开发方式?'),
-        choices:[{
-            value:'requirejs',
-            name:'使用(AMD)requirejs管理'
+    //前后端分离模式才走这
+    if(commander.separate){
+        inquirer.prompt([{
+            type:'list',
+            name:'jsmodule',
+            message:chalk.red.bgYellow('你使用的模块化开发方式?'),
+            choices:[{
+                value:'requirejs',
+                name:'使用(AMD)requirejs管理'
+            },{
+                value:'webpack',
+                name:'使用(Commonjs)webpack管理'
+            },{
+                value:'es6',
+                name:'使用es6 Module'
+            }],
+            default:0
         },{
-            value:'webpack',
-            name:'使用(Commonjs)webpack管理'
+            type:'list',
+            name:'cssmodule',
+            message:chalk.red.bgYellow('你使用的CSS预处理器？'),
+            choices:[{
+                value:'less',
+                name:'使用less预处理器管理CSS'
+            },{
+                value:'scss',
+                name:'使用sass预处理器管理CSS'
+            }],
+            default:0
         },{
-            value:'es6',
-            name:'使用es6 Module'
-        }],
-        default:0
-    },{
-        type:'list',
-        name:'cssmodule',
-        message:chalk.red.bgYellow('你使用的CSS预处理器？'),
-        choices:[{
-            value:'less',
-            name:'使用less预处理器管理CSS'
-        },{
-            value:'scss',
-            name:'使用sass预处理器管理CSS'
-        }],
-        default:0
-    },{
-        type:'list',
-        name:'version',
-        message:chalk.red.bgYellow('你使用的文件版本管理方式？'),
-        choices:[{
-            value:'vmd5',
-            name:'example.js?v=md5码,加search值版本管理方式'
-        },{
-            value:'rename',
-            name:'example.md5码.js,修改文件名的版本管理方式'
-        }],
-        default:0
-    }]).then(answers => {
-        if(isReplace){
-            shelljs.rm('-rf',existName);
-        }
-        downloadTemplate(name,answers);
-    })
+            type:'list',
+            name:'version',
+            message:chalk.red.bgYellow('你使用的文件版本管理方式？'),
+            choices:[{
+                value:'vmd5',
+                name:'example.js?v=md5码,加search值版本管理方式'
+            },{
+                value:'rename',
+                name:'example.md5码.js,修改文件名的版本管理方式'
+            }],
+            default:0
+        }]).then(answers => {
+            // if(isReplace){
+            //     shelljs.rm('-rf',existName);
+            // }
+            // downloadTemplate(name,answers,'wps-cli-dir');
+            downloadPro(name,answers,'wps-cli-dir');
+        });
+    }else{
+        inquirer.prompt([
+            {
+                type:'confirm',
+                name:'template',
+                message:chalk.red.bgYellow('是否需要HTML模块化功能'),
+                default:false
+            }
+        ]).then(answers => {
+            downloadPro(name,answers,'wps_mix');
+        });
+    }
 }
